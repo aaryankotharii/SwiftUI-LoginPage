6a5fd40e3ebd1acb69ffc95f9dc613b54573270e
aaryankotharii <aaryan.kothari@gmail.com>
2020-03-16 16:38:59 +0530

    signup + login + signout functions added

---
+++ SwiftUI- LoginPage/sessionStore.swift
@@ -0,0 +1,55 @@
+//
+//  sessionStore.swift
+//  SwiftUI- LoginPage
+//
+//  Created by Aaryan Kothari on 16/03/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import SwiftUI
+import Firebase
+import Combine
+
+class sessionStore: ObservableObject {
+    var didChange = PassthroughSubject<sessionStore, Never>()
+    @Published var session: User? { didSet { self.didChange.send(self)}}
+    var handle : AuthStateDidChangeListenerHandle?
+    
+    func listen(){
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            if let user = user {
+                self.session = User(uid: user.uid, email: user.email)
+            }else{
+                self.session = nil
+            }
+        })
+    }
+    
+    func signUp(email: String, Password: String, handler: @escaping AuthDataResultCallback){
+        Auth.auth().createUser(withEmail: email, password: Password, completion: handler)
+    }
+    
+    func signIn(email: String, password: String, handler: @escaping AuthDataResultCallback){
+        Auth.auth().signIn(withEmail: email, password: password, completion: handler)
+    }
+    
+    func signOut(){
+        do {
+            try Auth.auth().signOut()
+            self.session = nil
+        }catch{
+            print("Error signing out")
+        }
+    }
+    
+}
+
+struct User {
+    var uid: String
+    var email : String?
+    
+    init(uid:String, email:String?){
+        self.uid = uid
+        self.email = email
+    }
+}
